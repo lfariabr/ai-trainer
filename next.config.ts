1155b7065d0e96ed7b765e9baa7ebdfa19cce578
Luis Faria <lfariabr@gmail.com>
2025-05-19 14:26:54 +1000

    feat(homescreen) - adds Home page + footer + navbar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      'localhost:3000',
+      '127.0.0.1:3000',
+      '192.168.0.40:3000', 
+      '192.168.0.40'      
+    ]
+  }
 };
 
 export default nextConfig;
